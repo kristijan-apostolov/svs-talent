4531fbfa5eb21d568f4e4150d2f06cb335966f3f
Kristijan Apostolov <apostolovkristijan@gmail.com>
2015-03-12 23:35:15 +0100

    optimized version

--- 01/BoxVolume/src/BoxVolume.java
+++ 01/BoxVolume/src/BoxVolume.java
@@ -1,53 +1,28 @@
-public class BoxVolume implements Comparable {
+public class BoxVolume {
 
 	private int height;
 	private int weight;
 	private int depth;
 
 	public BoxVolume(int height, int weight, int depth) {
-	
+
 		this.height = height;
 		this.weight = weight;
 		this.depth = depth;
 	}
-
 	public int volume() {
-		return this.depth * this.weight *this.height;
+		return this.depth * this.weight * this.height;
 	}
 
 	public int getHeight() {
 		return height;
 	}
 
-	public void setHeight(int height) {
-		this.height = height;
-	}
-
 	public int getWeight() {
 		return weight;
-	}
-
-	public void setWeight(int weight) {
-		this.weight = weight;
 	}
 
 	public int getDepth() {
 		return depth;
 	}
-
-	public void setDepth(int depth) {
-		this.depth = depth;
-	}
-
-	public int compareTo(Object boxVolume) {
-		BoxVolume box = (BoxVolume) boxVolume;
-		if (this.volume() > box.volume()) {
-			return 1;
-		} else if (this.volume() < box.volume()) {
-			return -1;
-		} else {
-			return 0;
-		}
-	}
-
 }
