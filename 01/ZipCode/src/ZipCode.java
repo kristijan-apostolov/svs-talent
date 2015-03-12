9cd167be40198afe555ac52f9a8e7d4d01705b69
Kristijan Apostolov <apostolovkristijan@gmail.com>
2015-03-13 00:29:28 +0100

    optimized version

--- 01/ZipCode/src/ZipCode.java
+++ 01/ZipCode/src/ZipCode.java
@@ -3,15 +3,14 @@ public class ZipCode {
 	private int number;
 
 	public ZipCode(int number) {
-		if (lenght(number) == 5 || lenght(number) == 9) {
-			this.number = number;
-		} else {
-			System.err.println("number doesn't have 5 or 9 digits");
-		}
+		setNumber(number);
 	}
 
-	public int lenght(int x) {
-		int length = String.valueOf(x).length();
-		return length;
+	public void setNumber(int number) {
+		if ((number > 9999 && number < 100000)
+				|| (number > 99999999 && number < 1000000000))
+			this.number = number;
+		else
+			System.err.println("zip code is not valid, 5 or 9 digits!");
 	}
 }
