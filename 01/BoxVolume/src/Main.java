5ab685b204d7fa7915b798fbee852993dfe1748f
Kristijan Apostolov <apostolovkristijan@gmail.com>
2015-03-12 23:36:44 +0100

    optimized version

--- 01/BoxVolume/src/Main.java
+++ 01/BoxVolume/src/Main.java
@@ -1,29 +1,17 @@
-public class Main {
+public class BoxVolumeApplication {
 
 	public static void main(String[] args) {
 
 		BoxVolume box1 = new BoxVolume(2, 4, 3);
-		BoxVolume box2 = new BoxVolume(1, 4, 3);
-		
-		if (box1.compareTo(box2) == 1)
-		{
-			System.out.println("first box's volume is greater than second one");
-			System.out.println("height:" + box1.getHeight());
-			System.out.println("weight:" + box1.getWeight());
-			System.out.println("depth:" + box1.getDepth());
-		}
-		else if (box1.compareTo(box2) == -1)
-		{
-			System.out.println("second box's volume is greater than first one");
-			System.out.println("height:" + box2.getHeight());
-			System.out.println("weight:" + box2.getWeight());
-			System.out.println("depth:" + box2.getDepth());
-		}
-		else
-		{
-			System.out.println("Their volumes are equal");
-		}
+		BoxVolume box2 = new BoxVolume(8, 4, 3);
 
+		if (box1.volume() > box2.volume()) {
+			System.out.println("Height:" + box1.getHeight() + "\nWeight:"
+					+ box1.getWeight() + "\nDepth:" + box1.getDepth());
+		} else {
+			System.out.println("Height:" + box2.getHeight() + "\nWeight:"
+					+ box2.getWeight() + "\nDepth:" + box2.getDepth());
+		}
 	}
-
 }
+
